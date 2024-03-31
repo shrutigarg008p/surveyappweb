8a32881e1ee97fe08545a8db2c8d319595a51a9b
jitendrasingh <avani.32trikha@codeaxes.com>
2024-03-31 15:14:42 +0530

    Db

--- src/Config/db.config.js
+++ src/Config/db.config.js
@@ -1,5 +1,22 @@
+// module.exports = {
+// 	HOST: 'localhost',
+// 	USER: 'postgres',
+// 	PASSWORD: 'Password@123',
+//
+// 	DB: 'survey',
+// 	dialect: 'postgres',
+// 	pool: {
+// 		max: 5,
+// 		min: 0,
+// 		acquire: 30000,
+// 		idle: 10000,
+// 	},
+// };
+
+
+//Prod
 module.exports = {
-	HOST: 'localhost',
+	HOST: '20.198.255.72',
 	USER: 'postgres',
 	PASSWORD: 'Password@123',
 
@@ -12,21 +29,3 @@ module.exports = {
 		idle: 10000,
 	},
 };
-
-
-//Prod
-// module.exports = {
-// 	HOST: '20.198.255.72',
-// 	USER: 'postgres',
-// 	PASSWORD: 'Password@123',
-//
-// 	DB: 'isnew',
-// 	// DB: 'survey',
-// 	dialect: 'postgres',
-// 	pool: {
-// 		max: 5,
-// 		min: 0,
-// 		acquire: 30000,
-// 		idle: 10000,
-// 	},
-// };
